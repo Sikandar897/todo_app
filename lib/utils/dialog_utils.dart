b6dd4c5874bc2ed043f8cece9b590e31fb9d247b
Sikandar897 <sikandarafridi6@gmail.com>
2024-10-19 23:57:45 +0500

    add: dialog for messaging in utils

---
+++ lib/utils/dialog_utils.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Future<bool?> showConfirmationDialog(
+    BuildContext context, String title, String content) {
+  return showDialog<bool>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            child: const Text('No'),
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+          ),
+          TextButton(
+            child: const Text('Yes'),
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
